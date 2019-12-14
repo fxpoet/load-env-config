a6ba485b608e479b0622006ffe366170e5770b91
Kiyoung Jung <im@fxpoet.com>
2019-12-14 11:42:17 +0900

    v2 finished

--- index.js
+++ index.js
@@ -1,104 +1,145 @@
-let fs   = require('fs')
-let path = require('path')
-let dotenv = require('./dotenv.js')
-
+const fs     = require('fs')
+const path   = require('path')
+const dotenv = require('./dotenv.js')
+const yaml   = require('js-yaml')
 //-----------------------------------------------------------------
 
-// 여러 예상 경로 중에 파일 찾기
-function loadFile(filePathArray) {
-  if (typeof(filePathArray) == 'string')
-    filePathArray = [filePathArray];
+const default_fileList = [
+  'config/config.json',
+  'conf/config.json',
+  'conf/config.yml',
+  //'conf/config.development.yml', // auto load
+  'config.json',
+  'config.yml',
+  'config.env',
+  '.env'
+];
 
-  for (let i = 0; i < filePathArray.length; i++) {
-    if (fs.existsSync(path.resolve(process.cwd(), filePathArray[i])))
-      return filePathArray[i];
-  }
+const default_options = {
+  export: true, // 직접 env에 export
+  force: false  // export 할때 기존에 값이 있어도 강제로 쓰기
+};
 
-  return null;
-}
+const env_list = [
+  'production',
+  'prod',
+  'development',
+  'devel',
+  'test'
+]
 
-// Dotenv 코드 (.env 로드)
-function loadDotEnv(dotenvTarget) {
+function putEnvValue(key, value, cache, options) {
 
-  let dotenvPath = loadFile(dotenvTarget);
+  if (options.force != true) {
 
-  try {
-    const parsed = dotenv.parse(fs.readFileSync(dotenvPath, { encoding: 'utf8' }));
-    Object.keys(parsed).forEach(function (key) { process.env[key] = parsed[key]; })
-    return { parsed };
-  } catch (e) {
-    return { error: e };
-  }
-}
-
-function loadJSON(configJsonTarget) {
-  let file = loadFile(configJsonTarget)
-  if (!file) return;
-
-  let configs  = JSON.parse(fs.readFileSync(file, {encoding:'utf8'}));
-  let NODE_ENV = process.env.NODE_ENV || (configs.default && configs.default.NODE_ENV);
-
-  if (configs[NODE_ENV]) {
-    Object.keys(configs[NODE_ENV])
-    .forEach( key=>{
-      if (process.env[key]) return;
-      process.env[key] = configs[NODE_ENV][key];
-    })
-  }
-
-  if (configs.default) {
-    Object.keys(configs.default)
-    .forEach( key=>{
-      if (process.env[key]) return;
-      process.env[key] = configs.default[key];
-    })
-  }
-}
-
-// For Debug ----------------------------------------
-function _keepOldEnv() {
-  let o = {}
-  for (let i in process.env) {
-    o[i] = process.env[i];
-  }
-  return o;
-}
-
-function _compareOld(x) {
-  let filtered = {};
-
-  for (let i in process.env) {
-    if (x[i] == undefined) {
-      filtered[i] = process.env[i];
+    // 실행전 ENV에 등록된 거면 무시 (보통 Cross-env에서 셋팅된 것이나 컨테이너에서 부여한 것)
+    if (process.env[key]) {
+      if (cache[key] == undefined) return;
     }
   }
 
-  console.log(filtered);
+  if (typeof(value) != 'object') {
+
+    if (options.export)
+      process.env[key] = value;
+
+    cache[key] = value;
+  } else
+    cache[key] = (cache[key] == undefined) ? value : Object.assign(cache[key], value);
+
 }
+
+function loadFileJSON(filepath, cache, options) {
+  const configs = JSON.parse(fs.readFileSync(filepath, {encoding:'utf8'}));
+  Object.keys(configs).forEach( key=>{ putEnvValue(key, configs[key], cache, options) });
+  return filepath;
+}
+
+function loadFileENV(filepath, cache, options) {
+  const configs = dotenv.parse(fs.readFileSync(filepath, { encoding: 'utf8' }));
+  Object.keys(configs).forEach( key=>{ putEnvValue(key, configs[key], cache, options) });
+  return filepath;
+}
+
+function loadFileYML(filepath, cache, options) {
+  const configs = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
+  Object.keys(configs).forEach( key=>{ putEnvValue(key, configs[key], cache, options) });
+  return filepath;
+}
+
+function resolveFiles(file_list) {
+
+  let loadTarget = [];
+
+  file_list = file_list.map(i => path.resolve(process.cwd(), i)).filter(i => fs.existsSync(i))
+
+  // 디렉토리로 지정된 경우 안에 있는 리스트 뽑기
+  file_list.forEach(file => {
+    if (fs.lstatSync(file).isDirectory()) {
+      let subList = fs.readdirSync(file)
+      subList.sort( (a,b)=> (a.length - b.length))
+      subList.forEach(subItem=> {
+        if (fs.lstatSync(file+'/'+subItem).isFile())
+          loadTarget.push( path.resolve(process.cwd(), file+"/"+subItem) );
+      })
+    } else
+      loadTarget.push(file);
+  })
+
+  let env = (process.env.NODE_ENV) ? process.env.NODE_ENV : false;
+
+  // 로딩될 필요 없는 녀석들 제거
+  loadTarget = loadTarget.filter(file => {
+    let flag = (file.indexOf('example.') > -1) || (file[0] == "_");
+
+    env_list.forEach(v => {
+      if (v == env) return;
+      flag = flag || (file.indexOf(v+'.') > -1)
+    })
+
+    return !flag;
+  })
+
+  return loadTarget;
+}
+
 // ------------------------------------------------
+// 불러올 파일을 순서대로 정한다. 이왕이면 명시적인게 좋다.
+function config(loadFileList, options) {
 
-function config(overObject) {
+  if (Array.isArray(loadFileList) == false) {
+    options = loadFileList;
+    loadFileList = undefined;
+  }
 
-  //let old = _keepOldEnv() // For Debug
+  options = Object.assign(default_options, (options) ? options : {});
+  loadFileList = (loadFileList) ? loadFileList : default_fileList;
 
-  loadJSON([
-    'config.json',
-    'config/config.json',
-    'conf/config.json',
-    'env.config',
-    'config/env.config',
-    'conf/env.config'
-  ])
+  let cached = {};
+  let loadedFiles = [];
 
-  loadDotEnv([ '.env', 'config/.env', 'conf/.env' ])
+  resolveFiles(loadFileList).forEach(file=>{
 
-  if (overObject)
-    Object.keys(overObject)
-      .forEach( key=>{
-        process.env[key] = overObject[key];
-    });
+    let ext = path.extname(file);
 
-  //_compareOld(old) // For Debug
+    if (ext.toLowerCase() == '.json')
+      loadedFiles.push( loadFileJSON(file, cached, options) );
+
+    if (ext.toLowerCase() == '.yaml' || ext.toLowerCase() == '.yml')
+      loadedFiles.push( loadFileYML(file, cached, options) );
+
+    if (ext.toLowerCase() == '') {
+      let p = path.parse(file);
+      if (p.name == '.env') {
+        loadedFiles.push( loadFileENV(file, cached, options) );
+      }
+    }
+
+  })
+
+  loadedFiles = loadedFiles.filter(i => i);
+
+  return {order: loadedFiles, values: cached}
 }
 
 module.exports = config;
---
+++ test/index.spec.js
@@ -0,0 +1,70 @@
+
+// For Debug ----------------------------------------
+/*
+function _keepOldEnv() {
+    let o = {}
+    for (let i in process.env) {
+        o[i] = process.env[i];
+    }
+    return o;
+}
+
+function _compareOld(x) {
+    let filtered = {};
+
+    for (let i in process.env) {
+        if (x[i] == undefined) {
+            filtered[i] = process.env[i];
+        }
+    }
+
+    console.log(filtered);
+}
+*/
+
+test('basic', () => {
+
+    process.env.NODE_ENV = 'development'
+    let loadEnvConfig = require('../index.js')
+
+    let output = loadEnvConfig(['./test/config'], {export:false});
+
+    expect( output.order[0] ).toMatch("\\test\\config\\.env");
+    expect( output.order[1] ).toMatch("\\test\\config\\config.yml");
+    expect( output.order[2] ).toMatch("\\test\\config\\config.json");
+    expect( output.order[3] ).toMatch("\\test\\config\\config.development.json");
+
+    expect( output.values.server.port ).toBe(8000)
+
+})
+
+test('production', () => {
+
+    process.env.NODE_ENV = 'production'
+    let loadEnvConfig = require('../index.js')
+
+    let output = loadEnvConfig(['./test/config'], {export:false});
+
+    expect( output.order[0] ).toMatch("\\test\\config\\.env");
+    expect( output.order[1] ).toMatch("\\test\\config\\config.yml");
+    expect( output.order[2] ).toMatch("\\test\\config\\config.json");
+    expect( output.order[3] ).toMatch("\\test\\config\\config.production.yml");
+
+    expect( output.values.server.port ).toBe(80)
+
+})
+
+test('undefined env', () => {
+
+    process.env.NODE_ENV = undefined;
+    let loadEnvConfig = require('../index.js')
+
+    let output = loadEnvConfig(['./test/config'], {export:false});
+
+    expect( output.order[0] ).toMatch("\\test\\config\\.env");
+    expect( output.order[1] ).toMatch("\\test\\config\\config.yml");
+    expect( output.order[2] ).toMatch("\\test\\config\\config.json");
+
+    expect( output.values.server.port ).toBe(9000)
+
+})
